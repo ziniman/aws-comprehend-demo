8c6d9d2250c3dde5e2962f86f429367a6d3004c5
Boaz Ziniman <boazz@amazon.com>
2018-06-29 09:36:25 +0300

    Adding color coding

--- rss.py
+++ rss.py
@@ -1,10 +1,24 @@
 #!/usr/local/bin/python2.7
 
+#Reads a RSS feed and look for sentiment in title
+#Options feeds for demo
+#https://www.reddit.com/r/aws/.rss
+#https://stackoverflow.com/feeds/tag?tagnames=amazon-web-services&sort=newest
+#http://rss.cnn.com/rss/edition_world.rss
+
 import boto3
 import json
 import argparse
 import feedparser
 from time import sleep
+
+class bcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
 
 # command line arguments
 parser = argparse.ArgumentParser(description='Reads RSS feed and parse all items with comprehend')
@@ -23,17 +37,21 @@ for obj in feed.entries:
     title = (obj['title'])
     text = (obj['description'])
 
-    print('Calling DetectSentiment for %s' % title)
-    sentiment_response = comprehend.detect_sentiment(Text=title, LanguageCode='en')
+    if title:
+        print('Calling DetectSentiment for "%s"' % title)
+        sentiment_response = comprehend.detect_sentiment(Text=title, LanguageCode='en')
 
-    sentiment_score = sentiment_response["SentimentScore"]
-    Sentiment= str(sentiment_response['Sentiment'])
-    Positive = str(sentiment_score['Positive'])
-    Negative = str(sentiment_score['Negative'])
-    Neutral = str(sentiment_score['Neutral'])
-    Mixed = str(sentiment_score['Mixed'])
+        sentiment_score = sentiment_response["SentimentScore"]
+        Sentiment= str(sentiment_response['Sentiment'])
+        Positive = str(sentiment_score['Positive'])
+        Negative = str(sentiment_score['Negative'])
+        Neutral = str(sentiment_score['Neutral'])
+        Mixed = str(sentiment_score['Mixed'])
 
-    output = ('"%s", P"%s", NG"%s", NT"%s", M"%s"') % (Sentiment, Positive, Negative, Neutral, Mixed)
-    print (output)
-    print('End of DetectSentiment\n')
-    #sleep (5)
+        colors = {'POSITIVE': bcolors.GREEN, 'NEGATIVE': bcolors.RED, 'MIXED': bcolors.BLUE}
+        color = colors.get(Sentiment, bcolors.ENDC)
+
+        output = (color + '"%s", P"%s", NG"%s", NT"%s", M"%s"' + bcolors.ENDC) % (Sentiment, Positive, Negative, Neutral, Mixed)
+        print (output)
+        print('End of DetectSentiment\n')
+        sleep (1)
